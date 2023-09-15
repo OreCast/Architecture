d56cf12d5845f265d8ed61ff3a6d5f93facbbb66
Valentin Kuznetsov <vkuznet@gmail.com>
2023-09-15 19:07:33 -0400

    update

--- scripts/inject-test-data.sh
+++ scripts/inject-test-data.sh
@@ -3,14 +3,21 @@ echo "inject MetaData record"
 curl -X POST -H "Content-type: application/json" \
     -d '{"site":"cornell", "description": "waste minerals", "tags": ["waste", "minerals"], "bucket": "bucket"}' \
     http://localhost:8300/meta
+curl -X POST -H "Content-type: application/json" \
+    -d '{"site":"cornell", "description": "rare-materials", "tags": ["rare"], "bucket": "bucket"}' \
+    http://localhost:8300/meta
 echo
 echo "List of meta-data records"
 curl http://localhost:8300/meta
 echo
 
 echo "inject site record"
+url="http://127.0.0.1:8330"
+if [ "`hostname -s`" == "lnx15" ]; then
+    url="http://orecast-s3-dev.classe.cornell.edu:8330"
+fi
 curl -X POST -H "Content-type: application/json" \
-    -d '{"name":"cornell", "description": "Cornell waste mineral site", "url": "http://127.0.0.1:8330", "access_key": "79c936397f8846f67026104fc175908d29f4ff0f61d1e7da4102f46b2db499f3c4acf1e37c3d", "access_secret": "79c936397f8846f67026104fc175908d29f4ff0f61d1e7da4102f46b2db499f3c4acf1e37c3d", "use_ssl": false, "endpoint": "localhost:8330"}' \
+    -d '{"name":"cornell", "description": "Cornell waste mineral site", "url": $url, "access_key": "79c936397f8846f67026104fc175908d29f4ff0f61d1e7da4102f46b2db499f3c4acf1e37c3d", "access_secret": "79c936397f8846f67026104fc175908d29f4ff0f61d1e7da4102f46b2db499f3c4acf1e37c3d", "use_ssl": false, "endpoint": "localhost:8330"}' \
     http://localhost:8320/sites
 echo
 echo "List of site records"
