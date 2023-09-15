c2c416080c323cb5c12a6b7f123af26d30633d8f
Valentin Kuznetsov <vkuznet@gmail.com>
2023-09-15 13:15:56 -0400

    New scripts area to start services

---
+++ scripts/services.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+mkdir -p logs
+
+# delete existing web processes
+pid=`ps auxww | grep "\./web" | grep -v grep | awk '{print $2}' | awk '{print "kill -9 "$1""}'`
+if [ -n "$pid" ]; then
+    echo "$pid" | sed "s,\n,,g"
+    echo "$pid" | sed "s,\n,,g" | /bin/sh
+fi
+
+# start new processes
+for srv in Metadata Discovery Frontend
+do
+    echo "Start $srv service..."
+    cd $srv
+    nohup ./web 2>&1 1>& ../logs/$srv.log < /dev/null & \
+        echo $! > ../logs/$srv.pid
+    cd -
+done
