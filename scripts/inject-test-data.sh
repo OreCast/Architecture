847e7d86dcf7bc6d7df231261ae3011e3a394009
Valentin Kuznetsov <vkuznet@gmail.com>
2023-09-15 17:20:49 -0400

    update records

--- scripts/inject-test-data.sh
+++ scripts/inject-test-data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "inject MetaData record"
 curl -X POST -H "Content-type: application/json" \
-    -d '{"site":"cornell", "description": "bla-bla", "tags": ["tagA", "tagB"], "bucket": "bucket"}' \
+    -d '{"site":"cornell", "description": "waste minerals", "tags": ["waste", "minerals"], "bucket": "bucket"}' \
     http://localhost:8300/meta
 echo
 echo "List of meta-data records"
@@ -10,7 +10,7 @@ echo
 
 echo "inject site record"
 curl -X POST -H "Content-type: application/json" \
-    -d '{"name":"cornell", "url": "http://127.0.0.1:8330", "access_key": "79c936397f8846f67026104fc175908d29f4ff0f61d1e7da4102f46b2db499f3c4acf1e37c3d", "access_secret": "79c936397f8846f67026104fc175908d29f4ff0f61d1e7da4102f46b2db499f3c4acf1e37c3d", "use_ssl": false, "endpoint": "localhost:8330"}' \
+    -d '{"name":"cornell", "description": "Cornell waste mineral site", "url": "http://127.0.0.1:8330", "access_key": "79c936397f8846f67026104fc175908d29f4ff0f61d1e7da4102f46b2db499f3c4acf1e37c3d", "access_secret": "79c936397f8846f67026104fc175908d29f4ff0f61d1e7da4102f46b2db499f3c4acf1e37c3d", "use_ssl": false, "endpoint": "localhost:8330"}' \
     http://localhost:8320/sites
 echo
 echo "List of site records"
