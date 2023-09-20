6a817b7bb32709bba7495c5e627c08caf94341e8
Valentin Kuznetsov <vkuznet@gmail.com>
2023-09-20 10:32:56 -0400

    update

--- scripts/inject-test-data.sh
+++ scripts/inject-test-data.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "inject MetaData record"
 curl -X POST -H "Content-type: application/json" \
-    -d '{"site":"cornell", "description": "waste minerals", "tags": ["waste", "minerals"], "bucket": "waste"}' \
+    -d '{"site":"Cornell", "description": "waste minerals", "tags": ["waste", "minerals"], "bucket": "waste"}' \
     http://localhost:8300/meta
 curl -X POST -H "Content-type: application/json" \
-    -d '{"site":"cornell", "description": "rare-materials", "tags": ["rare"], "bucket": "minerals"}' \
+    -d '{"site":"Cornell", "description": "rare-materials", "tags": ["rare"], "bucket": "minerals"}' \
     http://localhost:8300/meta
 echo
 echo "List of meta-data records"
@@ -14,11 +14,11 @@ echo
 echo "inject site record"
 if [ "`hostname -s`" == "lnx15" ]; then
     curl -X POST -H "Content-type: application/json" \
-        -d '{"name":"cornell", "description": "Cornell waste mineral site", "url": "http://orecast-s3-dev.classe.cornell.edu:8330", "access_key": "79c936397f8846f67026104fc175908d29f4ff0f61d1e7da4102f46b2db499f3c4acf1e37c3d", "access_secret": "79c936397f8846f67026104fc175908d29f4ff0f61d1e7da4102f46b2db499f3c4acf1e37c3d", "use_ssl": false, "endpoint": "localhost:8330"}' \
+        -d '{"name":"Cornell", "description": "Cornell waste mineral site", "url": "http://orecast-s3-dev.classe.cornell.edu:8330", "access_key": "79c936397f8846f67026104fc175908d29f4ff0f61d1e7da4102f46b2db499f3c4acf1e37c3d", "access_secret": "79c936397f8846f67026104fc175908d29f4ff0f61d1e7da4102f46b2db499f3c4acf1e37c3d", "use_ssl": false, "endpoint": "localhost:8330"}' \
         http://localhost:8320/sites
 else
     curl -X POST -H "Content-type: application/json" \
-        -d '{"name":"cornell", "description": "Cornell waste mineral site", "url": "http://127.0.0.1:8330", "access_key": "79c936397f8846f67026104fc175908d29f4ff0f61d1e7da4102f46b2db499f3c4acf1e37c3d", "access_secret": "79c936397f8846f67026104fc175908d29f4ff0f61d1e7da4102f46b2db499f3c4acf1e37c3d", "use_ssl": false, "endpoint": "localhost:8330"}' \
+        -d '{"name":"Cornell", "description": "Cornell waste mineral site", "url": "http://127.0.0.1:8330", "access_key": "79c936397f8846f67026104fc175908d29f4ff0f61d1e7da4102f46b2db499f3c4acf1e37c3d", "access_secret": "79c936397f8846f67026104fc175908d29f4ff0f61d1e7da4102f46b2db499f3c4acf1e37c3d", "use_ssl": false, "endpoint": "localhost:8330"}' \
         http://localhost:8320/sites
 fi
 echo
