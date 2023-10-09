d005984489de224b5bb453d887eaf4061a88d56d
Valentin Kuznetsov <vkuznet@gmail.com>
2023-10-09 08:43:44 -0400

    Use data area to keep various data records; update inject script to use records from data area

--- scripts/inject-test-data.sh
+++ scripts/inject-test-data.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+sdir=$(dirname "$0")
+echo "Script dir: $sdir"
+ddir=$sdir/../data
+echo "Data dir: $ddir"
+
 echo "### get authz token"
 # curl -v "http://localhost:8380/oauth/token?client_id=client_id&client_secret=client_secret&grant_type=client_credentials&scope=read"
 token=`curl -s "http://localhost:8380/oauth/token?client_id=client_id&client_secret=client_secret&grant_type=client_credentials&scope=read" | jq '.access_token' | sed -e "s,\",,g"`
@@ -9,23 +14,29 @@ echo "token=$token"
 echo
 echo
 echo "### inject MetaData record 1"
+cat $ddir/meta/record1.json
+echo
 curl -v -X POST -H "Content-type: application/json" \
     -H "Authorization: Bearer $token" \
-    -d '{"site":"Cornell", "description": "waste minerals", "tags": ["waste", "minerals"], "bucket": "waste"}' \
+    -d@$ddir/meta/record1.json \
     http://localhost:8300/meta
 echo
 echo
 echo "### inject MetaData record 2"
+cat $ddir/meta/record2.json
+echo
 curl -v -X POST -H "Content-type: application/json" \
     -H "Authorization: Bearer $token" \
-    -d '{"site":"Cornell", "description": "rare-materials", "tags": ["rare"], "bucket": "minerals"}' \
+    -d@$ddir/meta/record2.json \
     http://localhost:8300/meta
 echo
 echo
 echo "### inject MetaData record 3"
+cat $ddir/meta/record3.json
+echo
 curl -v -X POST -H "Content-type: application/json" \
     -H "Authorization: Bearer $token" \
-    -d '{"site":"Mineville", "description": "waste materials", "tags": ["waste"], "bucket": "waste"}' \
+    -d@$ddir/meta/record3.json \
     http://localhost:8300/meta
 echo
 echo
@@ -39,35 +50,53 @@ if [ "`hostname -s`" == "lnx15" ]; then
     echo
     echo
     echo "### inject site record 1"
+    cat $ddir/sites/record1.json
+    echo
     curl -v -X POST -H "Content-type: application/json" \
         -H "Authorization: Bearer $token" \
-        -d '{"name":"Cornell", "description": "Cornell minerals site", "url": "http://orecast-s3-dev.classe.cornell.edu:8330", "access_key": "79c936397f8846f67026104fc175908d29f4ff0f61d1e7da4102f46b2db499f3c4acf1e37c3d", "access_secret": "79c936397f8846f67026104fc175908d29f4ff0f61d1e7da4102f46b2db499f3c4acf1e37c3d", "use_ssl": false, "endpoint": "localhost:8330"}' \
+        -d@$ddir/sites/record1.json \
         http://localhost:8320/sites
     echo
     echo
     echo "### inject site record 2"
+    cat $ddir/sites/record2.json
+    echo
     curl -v -X POST -H "Content-type: application/json" \
         -H "Authorization: Bearer $token" \
-        -d '{"name":"Mineville", "description": "Mineville waste site", "url": "http://orecast-s3-dev.classe.cornell.edu:8330", "access_key": "79c936397f8846f67026104fc175908d29f4ff0f61d1e7da4102f46b2db499f3c4acf1e37c3d", "access_secret": "79c936397f8846f67026104fc175908d29f4ff0f61d1e7da4102f46b2db499f3c4acf1e37c3d", "use_ssl": false, "endpoint": "localhost:8330"}' \
+        -d@$ddir/sites/record2.json \
         http://localhost:8320/sites
 else
     echo
     echo
     echo "### inject site record 1"
+    cat $ddir/sites/local1.json
+    echo
     curl -v -X POST -H "Content-type: application/json" \
         -H "Authorization: Bearer $token" \
-        -d '{"name":"Cornell", "description": "Cornell minerals site", "url": "http://127.0.0.1:8330", "access_key": "79c936397f8846f67026104fc175908d29f4ff0f61d1e7da4102f46b2db499f3c4acf1e37c3d", "access_secret": "79c936397f8846f67026104fc175908d29f4ff0f61d1e7da4102f46b2db499f3c4acf1e37c3d", "use_ssl": false, "endpoint": "localhost:8330"}' \
+        -d@$ddir/sites/local1.json \
         http://localhost:8320/sites
     echo
     echo
     echo "### inject site record 2"
+    cat $ddir/sites/local2.json
+    echo
     curl -v -X POST -H "Content-type: application/json" \
         -H "Authorization: Bearer $token" \
-        -d '{"name":"Mineville", "description": "Minevill waste site", "url": "http://127.0.0.1:8330", "access_key": "79c936397f8846f67026104fc175908d29f4ff0f61d1e7da4102f46b2db499f3c4acf1e37c3d", "access_secret": "79c936397f8846f67026104fc175908d29f4ff0f61d1e7da4102f46b2db499f3c4acf1e37c3d", "use_ssl": false, "endpoint": "localhost:8330"}' \
+        -d@$ddir/sites/local2.json \
         http://localhost:8320/sites
 fi
 echo
 echo
 echo "### List of site records"
 curl -v -H "Authorization: Bearer $token" http://localhost:8320/sites
+echo
+
+echo
+echo "### insert record into DBS system"
+cat $ddir/dbs/record1.json
+echo
+curl -v -X POST -H "Authorization: Bearer $token" \
+    -H "Content-type: application/json" \
+    -d@$ddir/dbs/record1.json \
+    http://localhost:8310/dataset
 echo
